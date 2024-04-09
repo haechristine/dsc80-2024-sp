4ebb5410dc4e7adbce707fa186551b31a9772d71
Christine Law <christine.law14013@gmail.com>
2024-04-09 14:23:50 -0700

    firstchange

--- labs/lab01/lab.py
+++ labs/lab01/lab.py
@@ -8,7 +8,7 @@ import numpy as np
 
 
 # ---------------------------------------------------------------------
-# QUESTION 0 g
+# QUESTION 0 
 # ---------------------------------------------------------------------
 
 
@@ -30,7 +30,13 @@ def consecutive_ints(ints):
 
 
 def median_vs_mean(nums):
-    ...
+    nums.sort()
+    if len(nums)%2 == 0:
+        median = (nums[len(nums)//2] + nums[(len(nums)//2)-1])/2
+    else:
+        median = nums[len(nums)//2]
+    mean = sum(nums)/len(nums)
+    return median <= mean
 
 
 # ---------------------------------------------------------------------
@@ -39,7 +45,10 @@ def median_vs_mean(nums):
 
 
 def n_prefixes(s, n):
-    ...
+    final_string = ''
+    for k in range(n):
+        final_string += s[:n-k]
+    return final_string
 
 
 # ---------------------------------------------------------------------
@@ -48,7 +57,22 @@ def n_prefixes(s, n):
 
 
 def exploded_numbers(ints, n):
-    ...
+    # list = []
+    # for j in range(len(ints)):
+    #     for k in range(n):
+    #         list += ints[j] - n
+    #         j+1
+    list = []
+    length = len(str(ints[-1] + n))
+    min_num = ints[0] - n
+    
+    for num in ints:
+        exploded = [str(i).zfill(length) for i in range(num - n, num + n + 1)]
+        list.append(' '.join(exploded))
+    
+    return list
+
+
 
 
 # ---------------------------------------------------------------------
