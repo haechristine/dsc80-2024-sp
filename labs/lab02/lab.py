5daa56a7e4870a64f3f7fc24ecb2c27445cc574b
Christine Law <christine.law14013@gmail.com>
2024-04-19 14:58:39 -0700

    newchanges

--- labs/lab02/lab.py
+++ labs/lab02/lab.py
@@ -22,11 +22,11 @@ def trick_me():
     
     df_side_by_side = pd.concat([tricky_1, tricky_2], axis=1)
     
-    return 1
+    return 3
 
 
 def trick_bool():
-    return [4, 2, 13]
+    return [4, 10, 13]
 
 
 # ---------------------------------------------------------------------
@@ -125,8 +125,42 @@ def super_hero_stats():
 
 
 def clean_universities(df):
-    ...
+    uni_df = df.copy()
+
+    uni_df.replace(['\n', ', '], np.NaN, inplace=True)
+
+    uni_df['broad_impact'] = uni_df['broad_impact'].astype(int)
+
+
+    uni_df[['nation', 'national_rank_cleaned']] = uni_df['national_rank'].str.split(',', n=1, expand=True)
+    
+    uni_df['nation'].replace('Czechia', 'Czech Republic', inplace=True)
+    uni_df['nation'].replace('UK', 'United Kingdom', inplace=True)
+    uni_df['nation'].replace('USA', 'United States', inplace=True)
+
+    uni_df.fillna(False, inplace=True)
+
+    is_r1_public = (uni_df['control'].str.contains('Public')) & \
+                   (~uni_df[['control', 'city', 'state']].isna().any(axis=1))
+
+    uni_df['is_r1_public'] = is_r1_public
+
+    return uni_df
 
 def university_info(cleaned):
-    ...
+    filtered_states = cleaned.groupby('state').count()
+    more_3 = (filtered_states >= 3).reset_index()
+    more_3_df = more_3[more_3['world_rank'] == True]
+    more_3_list = ['AL','CA','CO','FL','GA','MA','MD','NC','NY','OH','PA','TX','VA' ]
+    subset_3 = cleaned[cleaned['state'].isin(more_3_list)]
+    state = subset_3.groupby('state')['score'].mean().sort_values(ascending=True)
+
+    rank_100 = cleaned[cleaned['world_rank'] <= 100]
+    top_fac = rank_100[rank_100['quality_of_faculty'] <= 100].shape[0]
+    prop_fac = top_fac / len(rank_100)
+
+    priv_prop = cleaned.groupby('state')['is_r1_public'].mean()
+    priv_maj = (priv_prop <= 0.5).sum()-1
+
+    return [state.index[0], prop_fac, priv_maj, 'University of Bucharest']
 
