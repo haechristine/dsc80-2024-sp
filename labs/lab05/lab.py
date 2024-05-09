6ecfc60106894cd6387ab6f63e0934c7963eeef2
Christine Law <christine.law14013@gmail.com>
2024-05-08 23:18:30 -0700

    finishlab5?

--- labs/lab05/lab.py
+++ labs/lab05/lab.py
@@ -13,7 +13,8 @@ from scipy import stats
 
 
 def after_purchase():
-    ...
+    return ['NMAR','MD','MAR','NMAR','MAR']
+
 
 
 # ---------------------------------------------------------------------
@@ -22,7 +23,7 @@ def after_purchase():
 
 
 def multiple_choice():
-    ...
+    return ['MAR','MAR','MAR','NMAR','MCAR']
 
 
 # ---------------------------------------------------------------------
@@ -32,11 +33,11 @@ def multiple_choice():
 
 
 def first_round():
-    ...
+    return [0.0453, 'NR']
 
 
 def second_round():
-    ...
+    return [0.0235, 'R' ,'D']
 
 
 # ---------------------------------------------------------------------
@@ -45,7 +46,13 @@ def second_round():
 
 
 def verify_child(heights):
-    ...
+    p_vals = {}
+    for col in heights.columns:
+        if col not in ['father', 'child']:
+            p_vals[col] = (stats.ks_2samp(heights.loc[heights[col].isna(), 'father'],
+                                       heights.loc[heights[col].notna(), 'father']).pvalue)
+
+    return pd.Series(p_vals)
 
 
 # ---------------------------------------------------------------------
@@ -54,7 +61,13 @@ def verify_child(heights):
 
 
 def cond_single_imputation(new_heights):
-    ...
+    quartiles = pd.qcut(new_heights['father'], q=4, duplicates='drop')
+    
+    single_mean = new_heights.groupby(quartiles)['child'].transform('mean')
+    
+    impute = new_heights['child'].fillna(single_mean)
+    
+    return impute
 
 
 # ---------------------------------------------------------------------
@@ -63,11 +76,34 @@ def cond_single_imputation(new_heights):
 
 
 def quantitative_distribution(child, N):
-    ...
+    obs_vals = child.dropna().values
+
+    counter, bin_limit = np.histogram(obs_vals, bins=10, density=True)
+
+    bin_widths = np.diff(bin_limit)
+
+    area = counter * bin_widths
+
+    bin_index = np.random.choice(np.arange(len(area)), size=N, p=area)
+
+    impute_vals = []
+    for bin in bin_index:
+        lower = bin_limit[bin]
+        upper = bin_limit[bin + 1]
+        impute_vals.append(np.random.uniform(lower, upper))
+
+    return np.array(impute_vals)
 
 
 def impute_height_quant(child):
-    ...
+    missing_count = child.isna().sum()
+
+    impute_vals = quantitative_distribution(child, missing_count)
+
+    impute_child = child.copy()
+    impute_child[child.isna()] = impute_vals
+
+    return impute_child
 
 
 # ---------------------------------------------------------------------
@@ -76,4 +112,6 @@ def impute_height_quant(child):
 
 
 def answers():
-    ...
+    mc = [1, 2, 2, 1]
+    url = ['soundcloud.com','naver.com']
+    return mc,url
